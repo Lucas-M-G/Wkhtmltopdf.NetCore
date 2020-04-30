21cdbef8f048d0ee841b8a1303fc3ffcfbec2063
Lucas Gaitzsch <6965260+Lucas-M-G@users.noreply.github.com>
2020-04-30 22:12:01 +0200

    init: try multiple paths

--- Wkhtmltopdf.NetCore/WkhtmltopdfConfiguration.cs
+++ Wkhtmltopdf.NetCore/WkhtmltopdfConfiguration.cs
@@ -12,20 +12,25 @@ namespace Wkhtmltopdf.NetCore
 {
     public static class WkhtmltopdfConfiguration
     {
-        public static string RotativaPath { get; set; }
+        public static string RotativaPath { get; set; } = "";
 
         /// <summary>
         /// Setup Rotativa library
         /// </summary>
         /// <param name="services">The IServiceCollection object</param>
         /// <param name="wkhtmltopdfRelativePath">Optional. Relative path to the directory containing wkhtmltopdf. Default is "Rotativa". Download at https://wkhtmltopdf.org/downloads.html</param>
-        public static IServiceCollection AddWkhtmltopdf(this IServiceCollection services, string wkhtmltopdfRelativePath = "Rotativa")
+        public static IServiceCollection AddWkhtmltopdf(this IServiceCollection services, params string[] wkhtmltopdfRelativePath)
         {
-            RotativaPath = wkhtmltopdfRelativePath;
+            foreach(var path in wkhtmltopdfRelativePath) {
+                if (Directory.Exists(RotativaPath) || File.Exists(RotativaPath))
+                {
+                    RotativaPath = path;
+                    break;
+                }
+            }
 
-            if (!Directory.Exists(RotativaPath) && !File.Exists(RotativaPath))
-            {
-                throw new Exception("Folder containing wkhtmltopdf not found, searched for " + RotativaPath);
+            if (RotativaPath == "") {
+                 throw new Exception("Folder containing wkhtmltopdf not found, searched for " + RotativaPath);
             }
 
             var fileProvider = new UpdateableFileProvider();
